91fef9134791b2c908bd5f5bede4b3656d6a9f15
FriedHamCheese <160022244+FriedHamCheese@users.noreply.github.com>
2025-02-23 14:10:24 +0700

    Fixed ByteRegister premature overwriting by rising overwrite signal

--- DumbassCPU.srcs/sources_1/new/OneBitDFlipFlop.vhd
+++ DumbassCPU.srcs/sources_1/new/OneBitDFlipFlop.vhd
@@ -32,20 +32,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity OneBitDFlipFlop is Port(
-    bit_in: in std_logic;
-    rising_edge_clk: in std_logic;
+    bit_in, 
+    rising_edge_clk,
+    overwrite
+    : in std_logic;
     bit_out: out std_logic
 );
 end OneBitDFlipFlop;
 
 architecture Behavioral of OneBitDFlipFlop is
 
+signal stored_bit, overwrite_as: std_logic := '0';
+
 begin
+    bit_out <= stored_bit;
+    overwrite_as <= (overwrite and bit_in) or ((not overwrite) and stored_bit);
     proc: process(rising_edge_clk, bit_in) is
     begin
         if (rising_edge(rising_edge_clk)) then
-            bit_out <= bit_in;
+            stored_bit <= overwrite_as;
         end if;
     end process proc;
-    
 end Behavioral;
